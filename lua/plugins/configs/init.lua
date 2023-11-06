9149287a32e0eb4424cc275d205a6bbc8cca05ba
Nikita Meshchaninov <dev@nikita.ninja>
2023-11-06 23:42:31 +0400

    Delete deadcolumn

--- lua/plugins/configs/init.lua
+++ lua/plugins/configs/init.lua
@@ -1,7 +1,6 @@
 require("plugins.configs.telescope")
 require("plugins.configs.colorscheme")
 require("plugins.configs.nvim-tree")
-require("plugins.configs.deadcolumn")
 require("plugins.configs.cursorline")
 require("plugins.configs.treesitter")
 require("plugins.configs.harpoon")
