c2fbda25d3a09da2556daa2617018376ee758b2a
Nikita Meshchaninov <dev@nikita.ninja>
2023-11-07 23:44:01 +0400

    change lsp

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -11,5 +11,27 @@ require('mason-lspconfig').setup({
   ensure_installed = {"rust_analyzer"},
   handlers = {
     lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
   },
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+  },
+  formatting = lsp_zero.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-j>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-k>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
 })
